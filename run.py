1c3e79de94513c90d00fe89604c39c07feec1fa7
Woody Huang <woody1573@163.com>
2017-02-28 21:38:52 +0800

    initial commit
    Signed-off-by: Woody Huang <woody1573@163.com>

---
+++ run.py
@@ -0,0 +1,3 @@
+# 1. Detect the insertion of external drive and find if image inputs exits
+# 2. Build the motion.xml config file
+# 3. Copy the images and configuration file into the corresponding directory
